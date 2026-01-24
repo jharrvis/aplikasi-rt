841af247fb359035a914f2af30cabbced8de08c8
jharrvis <jharrvis.forwarded@gmail.com>
2026-01-24 12:34:11 +0700

    fix: update mobile footer link to use correct dashboard.wargas.index route

--- backend/resources/views/layouts/dashboard.blade.php
+++ backend/resources/views/layouts/dashboard.blade.php
@@ -230,7 +230,7 @@
                         d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                 </svg>
             </a>
-            <a href="{{ route('dashboard.wargas') }}"
+            <a href="{{ route('dashboard.wargas.index') }}"
                 class="flex flex-col items-center gap-1 {{ request()->is('dashboard/wargas*') ? 'text-primary-600' : 'text-slate-400' }}">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
